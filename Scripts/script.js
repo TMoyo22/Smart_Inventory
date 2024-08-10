8cb7fab25188e8fb02754a004d2709eea0f18ee5
Tatenda Moyo <131980229+TMoyo22@users.noreply.github.com>
2024-08-10 22:22:00 +0200

    building js for database

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,1 +1,9 @@
-const sign
+const loginInButton=document.getElementById('login');
+const loginForm=document.getElementById('loginform');
+
+loginInButton.addEventListener('click', function(){
+   loginForm.style.display="none";
+
+})
+
+      
